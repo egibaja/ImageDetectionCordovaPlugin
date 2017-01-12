e43b73097ed70d20933754c6f328c3b30b11a8bd
Eduardo Gibaja <eduardogibaja@gmail.com>
2017-01-12 20:36:14 +0100

    new scripts

--- python/orb.py
+++ python/orb.py
@@ -2,17 +2,20 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('t2.png',0)
+img = cv2.imread('t2b.png',0)
 
 # Initiate STAR detector
 # int nfeatures=500, float scaleFactor=1.2f, int nlevels=8, int edgeThreshold=31, int firstLevel=0, int WTA_K=2, int scoreType=ORB::HARRIS_SCORE, int patchSize=31, int fastThreshold=20
-orb = cv2.ORB_create(nfeatures=10000, edgeThreshold=3)
+orb = cv2.ORB_create(nfeatures=50, edgeThreshold=15, patchSize=15)
 
 # find the keypoints with ORB
 kp = orb.detect(img,None)
 
 # compute the descriptors with ORB
 kp, des = orb.compute(img, kp)
+
+
+print(kp)
 
 # draw only keypoints location,not size and orientation
 img2 = cv2.drawKeypoints(img, kp, None, color=(255,0,0), flags=0)
